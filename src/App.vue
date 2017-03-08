0eacaf6e11423ca895620f6a85c1a4d3c3c34d43
Sergey <grishy.ru@yandex.ru>
2017-03-08 13:24:33 +0300

    Init :godmode:

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+  #app
+    h1 Pug - node template engine
+      #container.col
+      i {{msg}}
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2
+  font-weight: normal
+  color: #f00
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
